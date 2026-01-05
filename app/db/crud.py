622c17f1ab36d53d18bd1bce5a9e60c86249e22c
IceDisel <tvv-vg@yandex.ru>
2026-01-05 19:40:52 +0300

    Refactor code

--- app/db/crud.py
+++ app/db/crud.py
@@ -10,32 +10,20 @@ from app.db.models import User, Subscription
 # Работа с пользователями
 # =========================
 
-async def get_user_by_tg_id(
-    session: AsyncSession,
-    tg_id: int
-) -> User | None:
+async def get_user_by_tg_id(session: AsyncSession, tg_id: int) -> User | None:
     """
     Получить пользователя по Telegram ID.
     Вернёт объект User или None.
     """
-    result = await session.execute(
-        select(User).where(User.tg_id == tg_id)
-    )
+    result = await session.execute(select(User).where(User.tg_id == tg_id))
     return result.scalar_one_or_none()
 
 
-async def create_user(
-    session: AsyncSession,
-    tg_id: int,
-    username: str | None
-) -> User:
+async def create_user(session: AsyncSession, tg_id: int, username: str | None) -> User:
     """
     Создаёт нового пользователя в БД.
     """
-    user = User(
-        tg_id=tg_id,
-        username=username
-    )
+    user = User(tg_id=tg_id, username=username)
     session.add(user)
     await session.commit()
     await session.refresh(user)
@@ -46,31 +34,18 @@ async def create_user(
 # Работа с подписками
 # =========================
 
-async def has_trial(
-    session: AsyncSession,
-    user_id: int,
-    vpn_type: str
-) -> bool:
+async def has_trial(session: AsyncSession, user_id: int, vpn_type: str) -> bool:
     """
     Проверяет, был ли у пользователя trial
     для конкретного vpn_type (например wireguard).
     """
     result = await session.execute(
-        select(Subscription).where(
-            Subscription.user_id == user_id,
-            Subscription.vpn_type == vpn_type,
-            Subscription.is_trial.is_(True)
-        )
-    )
+        select(Subscription).where(Subscription.user_id == user_id, Subscription.vpn_type == vpn_type,
+                                   Subscription.is_trial.is_(True)))
     return result.scalar_one_or_none() is not None
 
 
-async def create_trial_subscription(
-    session: AsyncSession,
-    user_id: int,
-    vpn_type: str,
-    days: int = 3
-) -> Subscription:
+async def create_trial_subscription(session: AsyncSession, user_id: int, vpn_type: str, days: int = 3) -> Subscription:
     """
     Создаёт trial-подписку на N дней.
     """
@@ -91,11 +66,7 @@ async def create_trial_subscription(
     return sub
 
 
-async def get_latest_subscription(
-    session,
-    user_id: int,
-    vpn_type: str
-) -> Subscription | None:
+async def get_latest_subscription(session, user_id: int, vpn_type: str) -> Subscription | None:
     """
     Возвращает ПОСЛЕДНЮЮ подписку пользователя
     для конкретного vpn_type.
@@ -105,14 +76,8 @@ async def get_latest_subscription(
     - нас интересует актуальная
     """
     result = await session.execute(
-        select(Subscription)
-        .where(
-            Subscription.user_id == user_id,
-            Subscription.vpn_type == vpn_type
-        )
-        .order_by(desc(Subscription.end_date))
-        .limit(1)
-    )
+        select(Subscription).where(Subscription.user_id == user_id, Subscription.vpn_type == vpn_type)
+        .order_by(desc(Subscription.end_date)).limit(1))
 
     return result.scalar_one_or_none()
 
@@ -128,18 +93,11 @@ def is_subscription_active(subscription: Subscription) -> bool:
     return subscription.end_date > now
 
 
-async def attach_wg_to_subscription(
-    session,
-    subscription_id: int,
-    public_key: str,
-    ip: str,
-):
+async def attach_wg_to_subscription(session, subscription_id: int, public_key: str, ip: str, ):
     """
     Сохраняет WireGuard-данные в подписке.
     """
-    result = await session.execute(
-        select(Subscription).where(Subscription.id == subscription_id)
-    )
+    result = await session.execute(select(Subscription).where(Subscription.id == subscription_id))
     sub = result.scalar_one()
 
     sub.wg_public_key = public_key
@@ -148,12 +106,7 @@ async def attach_wg_to_subscription(
     await session.commit()
 
 
-async def create_paid_subscription(
-    session,
-    user_id: int,
-    vpn_type: str,
-    days: int,
-):
+async def create_paid_subscription(session, user_id: int, vpn_type: str, days: int, ):
     """
     Создаёт платную подписку на N дней
     """
